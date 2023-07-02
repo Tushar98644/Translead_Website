e33adb4cdbee2a60089d670f2e5f9d6c2ec92ecf
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-02 13:20:20 +0530

    responsive navbar

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import { About, Contact, Home, Navbar, Team, Work } from "@/components";
+
+const Home_page = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Work />
+      <Team />
+      <Contact />
+    </>
+
+  );
+}
+
+export default Home_page;
