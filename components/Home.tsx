6be16db7e9b0b20bd63dad7c06d19dc26c529897
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-02 16:10:58 +0530

    responsive home section

--- components/Home.tsx
+++ components/Home.tsx
@@ -9,9 +9,9 @@ const Home = () => {
                 </Image>
                 <img alt='' src='/home-text.png' width={500} height={500} className='md:w-[45vw] md:h-[25vw] h-20 w-52 z-10 pt-0 absolute md:top-60 md:left-20 top-36 left-2 sm:h-[22vw] sm:w-[45vw] sm:left-20 sm:top-48' />
             </div>
-            <div className='flex flex-row md:gap-[30vw] w-half ml-12 md:my-40 gap-[10vw] text-sm mt-20'>
-                <span className='font-extrabold text-2xl md:text-4xl lg:text-5xl'>what problem are <br /> we solving?</span>
-                <div className='flex flex-col gap-8'>
+            <div className='flex flex-row md:gap-[15vw] w-half ml-12 md:my-40 gap-[10vw] text-sm mt-20'>
+                <span className='font-extrabold text-2xl md:text-4xl lg:text-5xl'>what problem are we solving?</span>
+                <div className='flex flex-col gap-8 mr-10'>
                     <span className='flex flex-row gap-3 items-center'>
                         <Image src='/1.png' width={20} height={20} alt='logo' className='w-8 h-8' />
                         <div className='flex flex-col gap-2'>
@@ -33,6 +33,18 @@ const Home = () => {
                             <p>Prolonged sitting can result in muscle stiffness, reduced flexibility, and weakened strength, making it harder to rise from a seated position.</p>
                         </div>
                     </span>
+                </div>
+            </div>
+            <div className='flex flex-row md:gap-[10vw] w-half ml-12 md:my-0 gap-[10vw] text-sm mt-20'>
+                <span className='font-extrabold text-2xl md:text-4xl lg:text-5xl md:my-0 my-40 '>How are we solving this <br />problem?</span>
+                <div className='flex flex-col gap-8'>
+                    <span className='md:text-4xl font-bold text-lg sm:text-3xl'>   STAND AT <span className='text-red-500'>EASE</span>
+                    </span>
+                    <div className='flex flex-col gap-10 md:flex-row pr-5'>
+                        <div> <Image src="/home-1.png" className='' width={200} height={200} alt="logo"></Image></div>
+                        <div> <Image src="/home-2.png" className='' width={200} height={200} alt="logo"></Image></div>
+                        <div> <Image src="/home-3.png" className='' width={200} height={200} alt="logo"></Image></div>
+                    </div>
                 </div>
             </div>
         </div>
