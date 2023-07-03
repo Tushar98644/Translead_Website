c204963106c64044d71760f0bc000670e39b6f8d
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-04 01:53:22 +0530

    contactmobile

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -14,13 +14,13 @@ const Contact = () => {
 
     
     return (
-        <div className="pt-36 flex flex-col sm:mx-24 mx-12 gap-4">
-            <div className="flex md:flex-row flex-col gap-20">
-                <div className="flex flex-col gap-4">
-                    <div className="sm:font-extrabold font-bold text-3xl  flex justify-start">
+        <div className="pt-36 flex flex-col sm:mx-24 mx-8 gap-4">
+            <div className="flex md:flex-row flex-col md:gap-20 gap-8">
+                <div className="flex flex-col gap-2">
+                    <div className="sm:font-extrabold font-bold sm:text-3xl text-lg  flex justify-start">
                         Contact
                     </div>
-                    <div className="text-lg">
+                    <div className="text-xs sm:text-lg">
                         For any inquiries about us and the work we do, please use the form on the right and drop a note to us. We will be sure to get back to you as soon as we can.
                     </div>
                 </div>
@@ -41,7 +41,7 @@ const Contact = () => {
                             <input type="text" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 focus:outline-none focus:ring-0 focus:border-blue-600 peer" onChange={(e)=>setSubject(e.target.value)} placeholder="Subject " required />
                         </div>
                         <div className="relative z-0 w-full mb-6 group">
-                            <textarea className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Write your message here" onChange={(e)=>setMessage(e.target.value)} rows={8} required />
+                            <textarea className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Write your message here" onChange={(e)=>setMessage(e.target.value)} rows={6} required />
                         </div>
                         <div className='text-center mt-12'>
                             <button type="submit" className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none shadow-xl focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-20 py-2.5 text-center">Submit</button>
@@ -50,14 +50,14 @@ const Contact = () => {
                 </div>
             </div>
             <div className="flex md:flex-row gap-10 flex-col mt-10">
-                <div className="flex flex-col gap-4">
-                    <div className="sm:font-extrabold font-bold text-3xl  flex justify-start">
+                <div className="flex flex-col gap-2">
+                    <div className="sm:font-extrabold font-bold sm:text-3xl text-lg  flex justify-start">
                         Our offices
                     </div>
-                    <div className="text-xl font-medium">
+                    <div className="sm:text-xl text-base font-medium">
                         Bangalore
                     </div>
-                    <div className="text-lg">
+                    <div className="sm:text-lg text-xs">
                         Translead Medtech Pvt Ltd,Incubated @ CPDMED TBI,MedTech & Geriatric Healthcare Technology Business Incubator @ Centre for Product Design & Manufacturing (CPDM),Raman Building,Indian Institute of Science (IISc),Bangalore-560012,Karnataka
                     </div>
                 </div>
