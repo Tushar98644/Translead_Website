18069ea73456b9bde72e397a21984279a5a450f0
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-03 12:55:19 +0530

    props types

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,8 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { ReactNode } from 'react';
 
-const Layout = ({children}) => {
+const Layout = ({children}:{children:ReactNode}) => {
     return ( 
         <div>
             <Navbar />
