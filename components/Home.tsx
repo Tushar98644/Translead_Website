c7ce07ff454fd6c065b0d592fdb3c8a57170601a
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-03 14:30:05 +0530

    homepage changes

--- components/Home.tsx
+++ components/Home.tsx
@@ -10,33 +10,61 @@ const Home = () => {
                 <img alt='' src='/Home/home-text.png' width={500} height={500} className='md:w-[45vw] md:h-[25vw] h-20 w-52 z-10 pt-0 md:absolute md:block md:top-60 md:left-20 hidden' />
                 <span className='absolute left-12 top-[60vw]  font-extrabold md:hidden'>Transforming Healthcare through Innovative Technology</span>
             </div>
-            <div className='flex md:flex-row flex-col md:gap-[10vw] w-half md:ml-12 mx-8 md:mt-28 md:mb-16 gap-[10vw] text-sm mt-20'>
+            <div className='flex md:flex-row flex-col md:gap-[10vw] w-half md:mx-12 mx-8 md:mt-28 md:mb-16 gap-8 text-sm mt-20'>
                 <span className='font-extrabold text-2xl md:text-4xl lg:text-5xl'>what problem are we solving?</span>
+                <div className="md:hidden items-start flex">
+                        <svg width="400" height="1" viewBox="0 0 738 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line y1="0.5" x2="738" y2="0.5" stroke="black" />
+                        </svg>
+                    </div>
                 <div className='flex flex-col gap-8 mr-10'>
-                    <span className='flex flex-row gap-3 items-center'>
+                    <span className='flex flex-row gap-3 items-start'>
                         <Image src='/Home/1.png' width={20} height={20} alt='logo' className='w-8 h-8' />
                         <div className='flex flex-col gap-2'>
                             <h1 className='text-xl'>Hindering Mobility</h1>
                             <p className='hidden md:block'>Prolonged sitting can result in muscle stiffness, reduced flexibility, and weakened strength, making it harder to rise from a seated position.</p>
                         </div>
                     </span>
-                    <span className='flex flex-row gap-3 items-center'>
+                    <div className="md:flex items-center hidden">
+                        <svg width="auto" height="1" viewBox="0 0 738 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line y1="0.5" x2="738" y2="0.5" stroke="black" />
+                        </svg>
+
+                    </div>
+                    <span className='flex flex-row gap-3 items-start'>
                         <Image src='/Home/2.png' width={20} height={20} alt='logo' className='w-8 h-8' />
                         <div className='flex flex-col gap-2'>
                             <h1 className='text-xl'>Hindering Mobility</h1>
                             <p className='hidden md:block'>Prolonged sitting can result in muscle stiffness, reduced flexibility, and weakened strength, making it harder to rise from a seated position.</p>
                         </div>
                     </span>
-                    <span className='flex flex-row gap-3 items-center'>
+                    <div className="md:flex items-start hidden">
+                        <svg width="auto" height="1" viewBox="0 0 738 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line y1="0.5" x2="738" y2="0.5" stroke="black" />
+                        </svg>
+                    </div>
+                    <span className='flex flex-row gap-3 items-start'>
                         <Image src='/Home/3.png' width={20} height={20} alt='logo' className='w-8 h-8' />
                         <div className='flex flex-col gap-2'>
                             <h1 className='text-xl'>Hindering Mobility</h1>
                             <p className='hidden md:block'>Prolonged sitting can result in muscle stiffness, reduced flexibility, and weakened strength, making it harder to rise from a seated position.</p>
                         </div>
                     </span>
+                    <div className="md:flex items-start hidden">
+                        <svg width="auto" height="1" viewBox="0 0 738 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line y1="0.5" x2="738" y2="0.5" stroke="black" />
+                        </svg>
+
+                    </div>
+                    <div className="md:hidden items-start flex">
+                        <svg width="400" height="1" viewBox="0 0 738 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line y1="0.5" x2="738" y2="0.5" stroke="black" />
+                        </svg>
+
+                    </div>
                 </div>
             </div>
-            <div className='flex md:flex-row flex-col md:gap-[13vw] w-half md:mx-12 mx-8 md:my-0 gap-[5vw] text-sm mt-16'>
+            <div className='flex md:flex-row flex-col md:gap-[13vw] w-half md:mx-12 mx-8 md:my-0 gap-[5vw] text-sm mt-12'>
                 <span className='font-extrabold text-2xl md:text-4xl lg:text-5xl md:my-20 my-0'>How are we solving this problem?</span>
                 <div className='flex flex-col gap-4 md:gap-8'>
                     <span className='md:text-4xl font-bold text-lg sm:text-3xl'>STAND AT <span className='text-red-500'>EASE</span>
