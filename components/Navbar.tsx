f8d7417d637915a46eba9624d5981b6d6d47be6e
Tushar98644 <b.toshar@iitg.ac.in>
2023-07-04 13:09:27 +0530

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isBlur, setIsBlur] = useState(false);
+    const [logoSrc, setLogoSrc] = useState("/Home/logo.png"); // New state variable for logo source
 
     useEffect(() => {
         const handleResize = () => {
@@ -13,8 +14,20 @@ const Navbar = () => {
             }
         };
 
+        const handleScroll = () => { // New scroll event handler
+            if (window.scrollY > 50) {
+                setLogoSrc("/Home/Translead Logo 1.png"); // Change to your other logo path
+            } else {
+                setLogoSrc("/Home/logo.png");
+            }
+        };
+
         window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
+        window.addEventListener('scroll', handleScroll); // Add scroll event listener
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            window.removeEventListener('scroll', handleScroll); // Remove scroll event listener
+        };
     }, []);
 
     return (
@@ -22,9 +35,9 @@ const Navbar = () => {
             <div className={`${isBlur ? 'fixed inset-0 bg-white bg-opacity-[85%] blur' : 'hidden'} md:hidden`} />
             <nav className="bg-white border-gray-200 border-b shadow-lg p-8 pt-4 md:pt-8 md:pb-14 pb-12  min-w-full fixed z-20 h-12 md:h-8">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-                    <Link href="/" className="flex items-center">
-                        <img src="/Home/logo.png" className="h-8 mr-3" alt="Flowbite Logo" />
-                    </Link>
+                        <Link href='/' className="flex items-center">
+                            <img src={logoSrc} className="h-8 mr-3" alt="Flowbite Logo" /> {/* Use logoSrc state variable */}
+                        </Link>
                     <button
                         onClick={() => {
                             setIsOpen(!isOpen);
@@ -42,19 +55,19 @@ const Navbar = () => {
                     <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
                             <li>
-                                <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-black md:hover:text-blue-700 md:p-0" aria-current="page">HOME</Link>
+                                    <Link href='/' className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-black md:hover:text-blue-700 md:p-0" aria-current="page">HOME</Link>
                             </li>
                             <li>
-                                <Link href="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">ABOUT US</Link>
+                                    <Link href='/about' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">ABOUT US</Link>
                             </li>
                             <li>
-                                <Link href="/product" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">OUR PRODUCT</Link>
+                                    <Link href="/product" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">OUR PRODUCT</Link>
                             </li>
                             <li>
-                            <Link href="/team" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">TEAM</Link>
+                                    <Link href='/team' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">TEAM</Link>
                             </li>
                             <li>
-                            <Link href="/contact" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">GET IN TOUCH</Link>
+                                    <Link href='/contact' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">GET IN TOUCH</Link>
                             </li>
                         </ul>
                     </div>
