fe6575c91f6239fb4e126bb25baa0535210d1983
Tushar98644 <b.toshar@iitg.ac.in>
2023-08-16 13:23:09 +0530

    About us page

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import { Fragment, useState } from 'react';
 import Map from './Map'
+import Image from 'next/image'
 
 const Contact = () => {
 
@@ -12,60 +14,73 @@ const Contact = () => {
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [showFailureMessage, setShowFailureMessage] = useState(false);
 
-    
+
     return (
-        <div className="md:pt-36 pt-24 flex flex-col sm:mx-10 mx-6 gap-4">
-            <div className="flex md:flex-row flex-col md:gap-20 gap-8">
-                <div className="flex flex-col gap-2">
-                    <div className="sm:font-extrabold font-bold sm:text-4xl text-lg  flex justify-start">
-                        Contact
+        <Fragment>
+            <div className="md:pt-36 pt-24 flex flex-col sm:mx-10 mx-6 gap-4">
+                <div className="flex md:flex-row flex-col md:gap-20 gap-8">
+                    <div className="flex flex-col gap-2">
+                        <div className="sm:font-extrabold font-bold text-4xl  flex justify-start">
+                            Contact
+                        </div>
+                        <div className="text-xl font-normal mt-2">
+                            For any inquiries about us and the work we do, please use the form on the right and drop a note to us. We will be sure to get back to you as soon as we can.
+                        </div>
+                        <div>
+                            <Image src='/contact/contact.gif' alt='image' height={500} width={500} />
+                        </div>
                     </div>
-                    <div className="text-sm sm:text-lg">
-                        For any inquiries about us and the work we do, please use the form on the right and drop a note to us. We will be sure to get back to you as soon as we can.
-                    </div>
-                </div>
-                <div className='w-full'>
-                    <form onSubmit={()=>console.log('hello')}>
-                        <div className="grid md:grid-cols-2 md:gap-6">
-                            <div className="relative z-0 w-full mb-6 group">
-                                <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" onChange={(e)=>setFirstName(e.target.value)} placeholder="First Name" required />
+                    <div className='w-full'>
+                        <form onSubmit={() => console.log('hello')}>
+                            <div className="grid md:grid-cols-2 md:gap-6">
+                                <div className="relative z-0 w-full mb-6 group">
+                                    <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-3 w-full text-base bg-[#F5F5F7] border border-[#64AFAC]  rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                        onChange={(e) => setFirstName(e.target.value)} placeholder="First Name" required />
+                                </div>
+                                <div className="relative z-0 w-full mb-6 group">
+                                    <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-3 w-full text-black bg-[#F5F5F7] border border-[#64AFAC]  rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                        onChange={(e) => setLastName(e.target.value)} placeholder="Last Name" required />
+                                </div>
                             </div>
                             <div className="relative z-0 w-full mb-6 group">
-                                <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" onChange={(e)=>setLastName(e.target.value)} placeholder="Last Name" required />
+                                <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-3 w-full text-black bg-[#F5F5F7] border border-[#64AFAC]  rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    onChange={(e) => setEmail(e.target.value)} placeholder="Email " required />
                             </div>
+                            <div className="relative z-0 w-full mb-6 group">
+                                <input type="text" name="floating_password" id="floating_password" className="block py-2.5 px-3 w-full text-black bg-[#F5F5F7] border border-[#64AFAC] rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    onChange={(e) => setSubject(e.target.value)} placeholder="Subject " required />
+                            </div>
+                            <div className="relative z-0 w-full mb-6 group">
+                                <textarea className="block py-2.5 px-3 w-full text-black bg-[#F5F5F7] border border-[#64AFAC]  rounded-lg focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder="Write your message here" onChange={(e) => setMessage(e.target.value)} rows={6} required />
+                            </div>
+                            <div className='w-full flex justify-center'>
+                                <button type="submit" className="block py-2 px-8 font-bold text-white border-none rounded-xl bg-[#FB1010] shadow-xl focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                >Submit</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <Image src='/website.png' alt='image' height={1300} width={1300} className='absolute opacity-40 -z-10 mx-0 md:top-[590px] top-[870px] rotate-180 w-full left-0' />
+                <div className="flex md:flex-row md:gap-20 gap-6 flex-col mt-10 ">
+                    <div className="flex flex-col gap-2">
+                        <div className=" font-black text-4xl flex justify-start">
+                            Our offices
                         </div>
-                        <div className="relative z-0 w-full mb-6 group">
-                            <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" onChange={(e)=>setEmail(e.target.value)} placeholder="Email " required />
+                        <div className="text-xl font-medium mt-12 mb-1">
+                            BANGALORE , INDIA
                         </div>
-                        <div className="relative z-0 w-full mb-6 group">
-                            <input type="text" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 focus:outline-none focus:ring-0 focus:border-blue-600 peer" onChange={(e)=>setSubject(e.target.value)} placeholder="Subject " required />
+                        <div className="text-lg font-light">
+                            Translead Medtech Pvt Ltd,Incubated @ CPDMED TBI,MedTech & Geriatric Healthcare Technology Business Incubator @ Centre for Product Design & Manufacturing (CPDM),Raman Building,Indian Institute of Science (IISc),Bangalore-560012,Karnataka
                         </div>
-                        <div className="relative z-0 w-full mb-6 group">
-                            <textarea className="block py-2.5 px-0 w-full text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Write your message here" onChange={(e)=>setMessage(e.target.value)} rows={6} required />
-                        </div>
-                        <div className='text-center mt-12'>
-                            <button type="submit" className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none shadow-xl focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-20 py-2.5 text-center">Submit</button>
-                        </div>
-                    </form>
+                    </div>
+                    <div className='w-[90vw] h-[50vw] md:h-[30vw] md:w-[150vw]'>
+                        <Map />
+                    </div>
                 </div>
             </div>
-            <div className="flex md:flex-row md:gap-20 gap-6 flex-col mt-10">
-                <div className="flex flex-col gap-2">
-                    <div className="sm:font-extrabold font-bold sm:text-4xl text-lg  flex justify-start">
-                        Our offices
-                    </div>
-                    <div className="sm:text-2xl text-base font-medium">
-                        Bangalore
-                    </div>
-                    <div className="sm:text-xl text-sm">
-                        Translead Medtech Pvt Ltd,Incubated @ CPDMED TBI,MedTech & Geriatric Healthcare Technology Business Incubator @ Centre for Product Design & Manufacturing (CPDM),Raman Building,Indian Institute of Science (IISc),Bangalore-560012,Karnataka
-                    </div>
-                </div>
-                <div className='w-[90vw] h-[50vw] md:h-[30vw] md:w-[150vw]'>
-                    <Map/>
-                </div>
-            </div>
-        </div>
+        </Fragment>
     );
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
+
+// const poppins = Poppins({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-poppins',
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main >
+      {/* <style jsx global>
+        {`
+          .font-100 { font-weight: 100; }
+          .font-200 { font-weight: 200; }
+          .font-300 { font-weight: 300; }
+          .font-400 { font-weight: 400; }
+          .font-500 { font-weight: 500; }
+          .font-600 { font-weight: 600; }
+          .font-700 { font-weight: 700; }
+          .font-800 { font-weight: 800; }
+          .font-900 { font-weight: 900; }
+        `}
+      </style> */}
+      <Component {...pageProps} />
+    </main>
+  );
 }
